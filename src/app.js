b200e869eb09135b3e2dc51f1b908e166bcae29f
galczo5 <kamil.galek@gmail.com>
2018-10-27 19:03:14 +0200

    added readme.org and license.org, added '--help' option

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
 #!/usr/bin/env node
 
+const PROGRAM_VERSION = '0.0.2'
 const program = require('commander');
 const moment = require('moment');
 const File = require('./file.js');
 const Entry = require('./entry.js');
 const Settings = require('./settings.js');
 const GLOBAL = require('./globals.js');
+const colors = require('colors');
 
-program.version('0.0.1')
-       .option('-d <dir>', 'set working directory')
-       .option('-h [12,24]', 'sets hour format, default 24')
-       .option('-f <date>', 'sets date, default: today')
-       .option('start <name>', 'starts new activity')
-       .option('stop', 'stops current activity')
-       .option('show', 'shows report')
-       .parse(process.argv);
+program.option('-d <dir>', '[required]'.bold + ' sets working directory')
+    .option('-h [12,24]', 'sets hour format, default 24')
+    .option('-f <date>', 'sets date, default: today')
+    .option('start <name>', 'starts new activity')
+    .option('stop', 'stops current activity')
+    .option('show', 'shows report')
+    .version(PROGRAM_VERSION, '-v, --version')
+    .parse(process.argv);
+
+if (!program.start && !program.stop && !program.show) {
+    program.help();
+    return;
+}
+
+if (!program.D) {
+    console.log('Please set working directory.');
+    console.log('Execute '+ '"time.txt --help"'.bold + ' to see manual.')
+    return;
+}
 
 GLOBAL.SETTINGS = new Settings(program.D, program.F, program.H);
-
-if (!program.start && !program.stop && !program.show)
-    return;
 
 let f = new File(GLOBAL.SETTINGS.filePath);
 f.load();
